a8a82bc85de9accabce457b1a22754f6310d47f5
vishal735 <vc009088@gmail.com>
2025-04-12 13:24:07 +0530

    first commit

---
+++ 001_basic/datatype.py
@@ -0,0 +1,36 @@
+# print("hello world")
+
+# a=12
+# b=12.2
+# b1=4j+5
+# c='A'
+# d=[1,2,3,4,5]
+# e=(1,2,3,4,5)
+# f={1,2,3,4,5}
+# g={"name":"vishal","surname":"chauhan"}
+# h=True
+# i=None
+# print(i)
+# print(h)
+# print(type(a))
+# print(type(b))
+# print(type(b1))
+# print(type(c))
+# print(type(d))
+# print(type(e))
+# print(type(f))
+# print(type(g))
+
+# a=input("enter name:") #input string user
+# b=int(input("enter number:")) #input number from user
+# print(a)
+# print(b)
+
+# print('''hello,my name is vishal
+#       i am from surat,
+#       i am developer''')
+
+# name="vishal"
+# surname="chauhan"
+# print(f'my name is {name} and my surname is {surname}')
+
